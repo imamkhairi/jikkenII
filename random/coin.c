426eab998fb3f0d086757456143b25f1cbc55b60
imamkhairi <imamkhairilubis@gmail.com>
2023-06-20 10:14:17 +0900

    done coin

---
+++ random/coin.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int rollDice() {
+    return rand() % 6;
+}
+
+int check(int dice) {
+    if (dice == 1 || dice == 2) return 1;
+    else return 0;
+}
+
+void giveCoin(int *dst, int *src) {
+    *dst = *dst + 1;
+    *src = *src - 1;
+}
+
+void startSimulation(int *A, int *B) {
+    // for (int i = 0; i < 20; i++) {
+    while(*A != 0 && *B != 0) {  
+        int dice = rollDice();
+        if (check(dice)) giveCoin(A, B);
+        else giveCoin(B, A);
+        printf("A = %d, B = %d\n", *A, *B);
+    }
+}
+
+void updateWinCount(int coin, int *winCount) {
+    if(coin) *winCount = *winCount + 1;
+    else return;
+}
+
+void resetCoin(int *A, int *B) {
+    *A = 6;
+    *B = 2;
+}
+
+void startCalculation(int *A, int *B, int *winA, int *winB, int iteration) {
+    for (int i = 0; i < iteration; i++) {
+        resetCoin(A, B);
+        startSimulation(A, B);
+        updateWinCount(*A, winA);
+        updateWinCount(*B, winB);
+        printf("=====\n");
+    }
+    printf("winA = %d, winB = %d\n", *winA, *winB);
+
+}
+
+
+int main(int argc, char **argv) {
+    srand(time(NULL));
+
+    int iteration;
+    if (argc == 2) iteration = atoi(argv[1]);
+    else {
+        printf("Set iteration count\n");
+        return 1;
+    }
+
+    int coinA = 6;
+    int coinB = 2;
+
+    int winA = 0;
+    int winB = 0;
+
+    startCalculation(&coinA, &coinB, &winA, &winB, iteration);
+
+    return 0;
+}
--- random/height.c
+++ random/height.c
@@ -6,31 +6,88 @@
 
 void allocateValue(int *dst) {
     for (int i = 0; i < SIZE; i++) {
-        dst[i] = i;
+        *dst = i;
+        dst++;
     }
 }
 
 void printData(int *dst) {
     for (int i = 0; i < SIZE; i++) {
-        printf("%d | value = %d\n", i, dst[i]);
+        printf("%d ", *dst);
+        dst++;
     }
+    printf("\n");
 }
 
-void swapElement(int )
+void swapElement(int *dst, int a, int b) {
+    int tmp = dst[a];
+    dst[a] = dst[b];
+    dst[b] = tmp;
+}
 
 void shuffle(int *dst) {
     for (int i = SIZE - 1; i >= 0; i--) {
-
+        int target = rand() % SIZE;
+        swapElement(dst, i, target);
     }
+}
+
+void combineValue(int *dst, int *m, int *w) {
+    for (int i = 0; i < SIZE; i++) {
+        *dst = *m;
+        dst++;
+        *dst = *w;
+        dst++;
+        
+        m++;
+        w++;
+    }
+}
+
+int check(int *dst) {
+    for (int i = 1; i < 2*SIZE; i += 2) {
+        if(dst[i-1] >= dst[i]) continue;
+        else return 0;
+    }
+    return 1;
+}
+
+void startCalculation(int iteration, int *result) {
+    int *m = (int *)malloc(SIZE * sizeof(int));
+    int *w = (int *)malloc(SIZE * sizeof(int));
+    int *combine = (int *)malloc(2*SIZE * sizeof(int));
+    allocateValue(m);
+    allocateValue(w);
+
+
+    for (int i = 0; i < iteration; i++) {
+        shuffle(m);
+        shuffle(w);
+        combineValue(combine, m, w);
+
+        // for(int j = 0; j < 10; j++) {
+        //     printf("%d ", combine[j]);
+        // }
+        *result += check(combine);
+            // printf("\nresult =  %d\n", *result);
+        // if(check(combine)) {
+        // }
+    }
+
+    free(m);
+    free(w);
+    free(combine);
 }
 
 int main(int argc, char **argv) {
     srand(time(NULL));
 
-    int *m = (int *)malloc(SIZE * sizeof(int));
-    int *w = (int *)malloc(SIZE * sizeof(int));
-    allocateValue(m);
-    allocateValue(w);
-    printData(m);
-    printData(w);
+    int iteration;
+
+    iteration = atoi(argv[1]);
+    int result = 0;
+
+    startCalculation(iteration, &result);
+    printf("result = %d\n", result);
+    
 }

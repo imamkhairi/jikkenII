117cec7744acdc0328a49ec54a974669d7241916
imamkhairi <imamkhairilubis@gmail.com>
2023-07-10 11:31:49 +0900

    stack done

--- netgraph/dfs.c
+++ netgraph/dfs.c
@@ -59,11 +59,34 @@ int searchRow(int *target, int size, int row, int *flag) {
     return offset;
 }
 
-void printFlag (int *flag, int size) {
+void print (int *target, int size) {
     for (int i = 0; i < size; i++) {
-        printf("[%d] : %d\n", i, *flag);
-        flag++;
+        printf("[%d] : %d\n", i, *target);
+        target++;
     }
+}
+
+int countFlag (int *flag, int size) {
+    int result = 0;
+    for (int i = 0; i < size; i++) {
+        result += flag[i];
+    }
+    return result;
+}
+
+void stackPush (int *stack, int data) {
+    while(*stack != 0) stack++;
+    *stack = data;
+}
+
+void stackPop (int *stack) {
+    while(*stack != 0) stack++;
+    *(--stack) = 0;
+}
+
+int stackTop (int *stack) {
+    while(*stack != 0) stack++;
+    return *(--stack);
 }
 
 int main(int argc, char **argv) {
@@ -78,31 +101,58 @@ int main(int argc, char **argv) {
 
     printf("Opended file: %s \n", fname);
 
-    char cr;
     int size = countLines(fp);
-
     int *data = (int *)malloc(size * size * sizeof(int));
-    int *flag = (int *)malloc(size * sizeof(int));
+    int *flag = (int *)malloc(size * sizeof(int)); // tandain udh ke sana atau belm. 1 udh. 0 blm
     int *result = (int *)malloc(size * sizeof(int));
+    int *stack = (int *)malloc(size * sizeof(int));
+    int currentNode = 0; // current node node index
 
-    // disini tambahin buat tracking index result
+    int *p = result; // pointer to accees result
 
-    // Awal harus gini (soalnya start dari 0)
-    flag[0] = 1;
-    result[0] = 0;
+    stackPush(stack, 69);
+    stackPush(stack, 10);
+    stackPush(stack, 25);
+    stackPop(stack);
+    stackPop(stack);
+    printf("Stack top: %d\n", stackTop(stack));
+    print(stack, size);
 
-    // printf("lines : %d\n", size);
-    storeArray(fp, data);
-    // printArray(data, size);
-    // printArrayAt(data, size, 10);
 
-    // misal udah jalan sekali, cuma blm updat resultnya
-    flag[searchRow(data, size, 0, flag)] = 1;
-    printf("first 1 in :%d\n", searchRow(data, size, 1, flag));
-    printFlag(flag, size);
+    /* Dari bawah sini jadi comment, jadi balikin lagi nanti*/
+
+    // // Initialize
+    // flag[currentNode] = 1;
+    // *p = currentNode;
+    // p++;
+    // // result[0] = currentNode;
+
+    // // printf("lines : %d\n", size);
+    // storeArray(fp, data);
+    // // printArray(data, size);
+    // // printArrayAt(data, size, 10);
+
+    // // misal udah jalan sekali, cuma blm updat resultnya
+    // currentNode = searchRow(data, size, currentNode, flag); //update currentNode node
+    // flag[currentNode] = 1;
+    // *p = currentNode;
+    // p++;
+
+    // currentNode = searchRow(data, size, currentNode, flag);
+    // flag[currentNode] = 1;
+    // *p = currentNode;
+    // p++;
+    // // for (int i = 0; i < size; i ++) {
+    // //     printf("first %d in :%d\n", i, searchRow(data, size, i, flag));
+    // // }
+
+    // // print(flag, size);
+    // print(result, size);
+    // printf("Flag sum : %d\n", countFlag(flag, size));
 
     free(data);
     free(flag);
     free(result);
+    free(stack);
     return 0;
 }
